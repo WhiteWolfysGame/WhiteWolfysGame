73b8aa5dc6b171a87c873a975fd76c181ed05a6d
WhiteWolfysGame <134242676+WhiteWolfysGame@users.noreply.github.com>
2023-07-16 21:21:53 +0200

    Anpassung an Navigation

--- js/script.js
+++ js/script.js
@@ -23,23 +23,25 @@ class Navigation{
    constructor(){
       this.page = new Array();
 
-      this.AddPage("Startseite", "index.html");
-      this.AddPage("Werdegang", "werdegang.html");
-      this.AddPage("Projektliste", "projektliste.html");
-      //this.AddPage("Kenntnisse", "kenntnisse.html");
+      this.AddPage("Startseite", "index.html", "", "", true);
+      this.AddPage("Werdegang", "werdegang.html", "", "", true);
+      this.AddPage("Projektliste", "projektliste.html", "", "", true);
+      //this.AddPage("Kenntnisse", "kenntnisse.html", "", "", true);
 
-      this.AddPage("Hobbies", "#", "YouTube", "youtube.html");
+      this.AddPage("Hobbies", "#", "Wolf", "wolf.html", false);
       this.AddPage("Hobbies", "#", "---");
-      this.AddPage("Hobbies", "#", "Twitch", "twitch.html");
+      this.AddPage("Hobbies", "#", "YouTube", "youtube.html", true);
+      this.AddPage("Hobbies", "#", "Twitch", "twitch.html", false);
    }
 
-   AddPage(pagename, url, subpagename, subpageurl){
+   AddPage(pagename, url, subpagename, subpageurl, enabled){
+      // Gleiche Pagenames werden zusammengeführt, sofern diese existieren
       var page = this.page.find(o => o.name === pagename);
 
       if (page != null)
-         page.subpages.push({name:subpagename, url:subpageurl});
+         page.subpages.push({name:subpagename, url:subpageurl, enabled:enabled});
       else
-         this.page.push({name:pagename, url:url, subpages:[{name:subpagename, url:subpageurl}]});
+         this.page.push({name:pagename, url:url, subpages:[{name:subpagename, url:subpageurl, enabled:enabled}], enabled:enabled});
    }
 
    BuildNav(currentPage, currentSubpage){
@@ -49,7 +51,7 @@ class Navigation{
             html += this.#DropdownNavItem(currentPage, this.page[i], currentSubpage);
          }
          else{
-            html += this.#SimpleNavItem(currentPage, this.page[i].name, this.page[i].url);
+            html += this.#SimpleNavItem(currentPage, this.page[i]);
          }
       }
 
@@ -79,11 +81,16 @@ class Navigation{
          }
          else
          {
-            if(currentSubpage == page.subpages[j].name){
-               html += '   <li><a class="dropdown-item active" aria-current="page" href="' + page.subpages[j].url + '">' + page.subpages[j].name + '</a></li>';
+            if(page.subpages[j].enabled){
+               if(currentSubpage == page.subpages[j].name){
+                  html += '   <li><a class="dropdown-item active" aria-current="page" href="' + page.subpages[j].url + '">' + page.subpages[j].name + '</a></li>';
+               }
+               else {
+                  html += '   <li><a class="dropdown-item" href="' + page.subpages[j].url + '">' + page.subpages[j].name + '</a></li>';
+               }
             }
             else {
-               html += '   <li><a class="dropdown-item" href="' + page.subpages[j].url + '">' + page.subpages[j].name + '</a></li>';
+               html += '   <li><a class="dropdown-item disabled">' + page.subpages[j].name + '</a></li>';
             }
          }
       }
@@ -93,13 +100,18 @@ class Navigation{
       return html;
    }
 
-   #SimpleNavItem(currentPage, pagename, url){
+   #SimpleNavItem(currentPage, page){
       let html = '<li class="nav-item">';
-      if(currentPage == pagename){
-         html += '   <a class="nav-link active" aria-current="page" href="' + url + '">' + pagename + '</a>';
+      if(page.enabled){
+         if(currentPage == page.name){
+            html += '   <a class="nav-link active" aria-current="page" href="' + page.url + '">' + page.name + '</a>';
+         }
+         else {
+            html += '   <a class="nav-link" href="' + page.url + '">' + page.name + '</a>';
+         }
       }
       else {
-         html += '   <a class="nav-link" href="' + url + '">' + pagename + '</a>';
+         html += '   <a class="nav-link disabled">' + page.name + '</a>';
       }
       html += '</li>';
 
