ca2a55cc8f850969bf9322199b50c29f393ad8fc
White Wolfy's Game <134242676+WhiteWolfysGame@users.noreply.github.com>
2024-02-09 12:18:29 +0100

    Überarbeitung Darstellung und Bugfixes

--- Typo3/php/wishlist.php
+++ Typo3/php/wishlist.php
@@ -19,35 +19,102 @@ function GetPlayedGametitle($g){
 }
 
 for($i = 0; $i < count($games); $i++){
+   if($i < 10){
+      ?>
+      <div class="row d-none d-sm-flex">
+      
+         <div class="col-2 col-xl-1 col-md-2">
+            <?=$games[$i]->votes?> Votes
+         </div>
+
+         <div class="col-10 col-xl-11 col-md-10">
+            <?=GetPlayedGametitle($games[$i])?>
+            <span class="badge bg-secondary"><?=$games[$i]->game->plattform->short_desc?></span>
+            <?php
+            if($games[$i]->game->is_digital){
+               ?>
+               <span class="badge bg-secondary">Digital</span>
+               <?php
+            }
+
+            if($games[$i]->game->is_collection){
+               ?>
+               <span class="text-secondary">[<?=$games[$i]->game->title?>]</span>
+               <?php
+            }
+            ?>
+         </div>
+
+         <div class="col-12 d-md-none"><br></div>
+
+      </div>
+
+      <?php
+   }
+   else{
+      if($i==10){
+         ?>
+         <br />
+
+         <div class="accordion">
+            <div class="accordion-item">
+               <h2 id="heading-<?=$i?>" class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?=$i?>" aria-expanded="true" aria-controls="collapse<?=$i?>">
+                     Weitere Titel...
+                  </button>               
+               </h2>
+               <div id="collapse<?=$i?>" class="accordion-collapse collapse" aria-labelledby="heading-<?=$i?>" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+
+
+            <br />
+         <?php
+      }
+      ?>
+      
+      <div class="row d-none d-sm-flex">
+      
+         <div class="col-2 col-xl-1 col-md-2">
+            <?=$games[$i]->votes?> Votes
+         </div>
+
+         <div class="col-10 col-xl-11 col-md-10">
+            <?=GetPlayedGametitle($games[$i])?>
+            <span class="badge bg-secondary"><?=$games[$i]->game->plattform->short_desc?></span>
+            <?php
+            if($games[$i]->game->is_digital){
+               ?>
+               <span class="badge bg-secondary">Digital</span>
+               <?php
+            }
+
+            if($games[$i]->game->is_collection){
+               ?>
+               <span class="text-secondary">[<?=$games[$i]->game->title?>]</span>
+               <?php
+            }
+            ?>
+         </div>
+
+         <div class="col-12 d-md-none"><br></div>
+
+      </div>
+
+      <?php
+      if($i==count($games)){
+         ?>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <?php
+      }
+      ?>
+
+      <?php
+   }
    ?>
 
-   <div class="row d-none d-sm-flex">
-   
-      <div class="col-2 col-xl-1 col-md-2">
-         <?=$games[$i]->votes?> Votes
-      </div>
-
-      <div class="col-10 col-xl-11 col-md-10">
-         <?=GetPlayedGametitle($games[$i])?>
-         <span class="badge bg-secondary"><?=$games[$i]->game->plattform->short_desc?></span>
-         <?php
-         if($games[$i]->game->is_digital){
-            ?>
-            <span class="badge bg-secondary">Digital</span>
-            <?php
-         }
-
-         if($games[$i]->game->is_collection){
-            ?>
-            <span class="text-secondary">[<?=$games[$i]->game->title?>]</span>
-            <?php
-         }
-         ?>
-      </div>
-
-      <div class="col-12 d-md-none"><br></div>
-
-   </div>
 
    <div class="row d-sm-none">
       <!-- Mobile Darstellung -->
@@ -89,7 +156,6 @@ for($i = 0; $i < count($games); $i++){
 
 ?>
 
-<br />
 
 
 <br/>
